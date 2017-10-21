09d37dc53c8425a36c09cf8fe57416c05345f3d1
Jason Toney <jason.toney.0598@mail.linnbenton.edu>
2017-10-20 21:20:08 -0700

    stuff

--- mklb.sh
+++ mklb.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
 cd $(dirname $0)
+PRESEED_CFG='config/includes.installer/preseed.cfg'
 
-[ -d .local ] || mkdir -p .local
-
-if [ -e .local/lb.cfg ] && [ -e config/includes.installer/preseed.template ]; then
+preseed_config() {
 	source .local/lb.cfg
-	cp -f config/includes.installer/preseed.template config/includes.installer/preseed.cfg
+	[ -n "$PRESEED_HOSTNAME" ] && [ -n "$PRESEED_USERNAME" ] &&
+		[ -n "$PRESEED_USERFULLNAME" ] && [ -n "$PRESEED_USERPASSWORD" ] &&
+			cp -f config/includes.installer/preseed.template "$PRESEED_CFG" || return
 
-	[ -n "$PRESEED_HOSTNAME" ] && sed -i "s|HOSTNAME|$PRESEED_HOSTNAME|" config/includes.installer/preseed.cfg
-	[ -n "$PRESEED_USERNAME" ] && sed -i "s|USERNAME|$PRESEED_USERNAME|" config/includes.installer/preseed.cfg
-	[ -n "$PRESEED_USERFULLNAME" ] && sed -i "s|USERFULLNAME|$PRESEED_USERFULLNAME|" config/includes.installer/preseed.cfg
-	[ -n "$PRESEED_USERPASSWORD" ] && sed -i "s|USERPASSWORD|$PRESEED_USERPASSWORD|" config/includes.installer/preseed.cfg
-
-	unset PRESEED_HOSTNAME PRESEED_USERNAME PRESEED_USERFULLNAME PRESEED_USERPASSWORD
-	PRESEEDED=true
-fi
+	sed -i "s|HOSTNAME|$PRESEED_HOSTNAME|" "$PRESEED_CFG"
+	sed -i "s|USERNAME|$PRESEED_USERNAME|" "$PRESEED_CFG"
+	sed -i "s|USERFULLNAME|$PRESEED_USERFULLNAME|" "$PRESEED_CFG"
+	sed -i "s|USERPASSWORD|$PRESEED_USERPASSWORD|" "$PRESEED_CFG"
+}
+[ -e .local/lb.cfg ] && preseed_config
 
 if [ $# == 0 ]; then ARGS="clean config bootstrap chroot installer binary" ; else ARGS="$@" ; fi
-
 for ARG in $ARGS ; do lb $ARG ; done
 
-[ $PRESEEDED ] && rm -f config/includes.installer/preseed.cfg
+[ -e config/includes.installer/preseed.template ] && rm -f "$PRESEED_CFG"
