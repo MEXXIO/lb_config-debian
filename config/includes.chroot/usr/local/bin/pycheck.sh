cb8a5b59ac7920acabf91231ffd580681dafafe8
Jason Toney <jason.toney.0598@mail.linnbenton.edu>
2019-11-08 15:51:36 -0800

    pycheck.sh: allow input file specification

--- config/includes.chroot/usr/local/bin/pycheck.sh
+++ config/includes.chroot/usr/local/bin/pycheck.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-# Generate an error report for all python3 scripts in the current directory
+# Generate an error report for python3 scripts
 
-for FILE in *.py
-do
+pycheck()
+{
+    local FILE="$1"
     REPORT=$(echo "$FILE" | sed 's|.py||')_report.txt
     echo "*** File: $FILE ***\n" > "$REPORT"
     for CHECK in pylint3 pycodestyle pydocstyle black
@@ -21,4 +22,16 @@ do
         sh -c "$CHECK $OPTS $FILE" >> "$REPORT"
         echo "==========================================" >> "$REPORT"
     done
+}
+
+if [ "$#" -eq 0 ]
+then
+    FILES="*.py"
+else
+    FILES="$@"
+fi
+
+for FILE in $FILES
+do
+    pycheck "$FILE"
 done
